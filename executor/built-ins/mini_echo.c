46e8ff8bcbb6b5362a89f579d50faf957971892b
nelisabe <aleksandr.engoyan@gmail.com>
2020-11-25 13:42:29 +0300

    mini_echo -nnnnnn -n -n -n many flags fix

--- executor/built-ins/mini_echo.c
+++ executor/built-ins/mini_echo.c
@@ -6,25 +6,47 @@
 /*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 13:25:40 by nelisabe          #+#    #+#             */
-/*   Updated: 2020/11/22 14:22:16 by nelisabe         ###   ########.fr       */
+/*   Updated: 2020/11/25 13:42:14 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../executor.h"
 
-int		mini_echo(char **args)
+static int	check_flag(char *argument)
 {
 	int		index;
+
+	index = -1;
+	if (argument[++index] != '-')
+		return (0);
+	while (argument[++index])
+		if (argument[index] != 'n')	
+			return (0);
+	if (argument[index - 1] == '-')
+		return (0);
+	return (1);
+}
+
+int			mini_echo(char **args)
+{
+	int		index;
+	int		temp;
 	int		flag;
 	
 	errno = 0;
-	flag = args[1] && !ft_strncmp("-n", args[1], 3);
-	index = flag;
+	index = 0;
+	flag = 0;
 	while (args[++index])
+		if (!(temp = check_flag(args[index])))
+			break ;
+		else
+			flag = temp;
+	while (args[index])
 	{
 		write(1, args[index], ft_strlen(args[index]));
 		if (args[index + 1])
 			write(1, " ", 1);
+		index++;
 	}
 	if (!flag)
 		write(1, "\n", 1);
