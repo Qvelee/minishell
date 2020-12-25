f6b6df7c4fb6e2ea2c516d99122179489125f689
nelisabe <aleksandr.engoyan@gmail.com>
2020-12-25 21:21:10 +0300

    signals codes added

--- executor/exec_utils.c
+++ executor/exec_utils.c
@@ -3,15 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   exec_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgertrud <msnazarow@gmail.com>             +#+  +:+       +#+        */
+/*   By: nelisabe <nelisabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 14:42:11 by nelisabe          #+#    #+#             */
-/*   Updated: 2020/12/25 00:12:10 by sgertrud         ###   ########.fr       */
+/*   Updated: 2020/12/25 21:16:46 by nelisabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "executor.h"
+
+static int	wait_process(t_exec *exec)
+{
+	int		error;
+
+	error = 0;
+	if ((waitpid(exec->pids[exec->index], &exec->status, \
+												WUNTRACED) == -1))
+		error_print_return(NULL);
+	while (!WIFEXITED(exec->status) && !WIFSIGNALED(exec->status))
+		if ((waitpid(exec->pids[exec->index], &exec->status, \
+									WUNTRACED) == -1))
+		error_print_return(NULL);
+	if (WIFSIGNALED(exec->status))
+	{
+		if (WTERMSIG(exec->status) == 2)
+			exec->return_value = 130;
+		if (WTERMSIG(exec->status) == 3)
+			exec->return_value = 131;
+	}
+	else if (check_fatal_error((exec->return_value = \
+					WEXITSTATUS(exec->status))))
+			error = exec->return_value;
+	return (error);
+}
 
 static int	wait_all_processes(t_exec *exec)
 {
@@ -23,17 +47,7 @@ static int	wait_all_processes(t_exec *exec)
 		if (exec->pids[exec->index] < 0)
 		{
 			exec->pids[exec->index] *= -1;
-			if ((waitpid(exec->pids[exec->index], &exec->status, \
-												WUNTRACED) == -1))
-
-				error_print_return(NULL);
-			while (!WIFEXITED(exec->status) && !WIFSIGNALED(exec->status))
-				if ((waitpid(exec->pids[exec->index], &exec->status, \
-					WUNTRACED) == -1))
-					error_print_return(NULL);
-			if (check_fatal_error((exec->return_value = \
-					WEXITSTATUS(exec->status))))
-				error = exec->return_value;
+			error = wait_process(exec);
 		}
 		else
 			exec->return_value = exec->pids[exec->index];
